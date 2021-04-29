35f2ff93a0a356e8f2a8c4b2d18a20b933481e6e
evan-mitchell93 <30278906+evan-mitchell93@users.noreply.github.com>
2021-04-29 15:28:07 -0400

    delete node from binary tree
    delete node from binary tree  and changed printTree to be postOrder instead of inOrder

--- BinaryTree/MyBinaryTree.java
+++ BinaryTree/MyBinaryTree.java
@@ -43,6 +43,66 @@ class MyBinaryTree {
     }
 
 
+    //delete data
+
+    void deleteData(int data){
+        root = deleteDataRec(root, data);
+    }
+
+    Node deleteDataRec(Node root,int data){
+
+        //base case empty tree
+        if(root == null){
+            return root;
+        }
+
+        //traverse tree to find the data value
+        if(data < root.data){
+            root.left = deleteDataRec(root.left, data);
+        }
+        else if(data > root.data){
+            root.right = deleteDataRec(root.right, data);
+        }
+
+
+        //key is found
+        else {
+            if(root.left == null){
+                return root.right;
+            }
+            else if (root.right == null){
+                return root.left;
+            }
+
+            //two children of the found node
+            //find smallest number on the right.
+            //replace root with that data
+            //then delete the replacement data from the right
+            root.data = minimumRight(root.right);
+
+            root.right = deleteDataRec(root.right, root.data);
+        }
+
+
+
+
+
+        return root;
+    }
+
+    int minimumRight(Node root){
+        int minimumValue = root.data;
+
+        //smallest value is the furthest left
+        while(root.left != null){
+            minimumValue  = root.left.data;
+            root = root.left;
+        }
+
+        return minimumValue;
+    }
+
+
     void printTree(){
         printTreeRec(root);
     }
@@ -51,10 +111,11 @@ class MyBinaryTree {
     void printTreeRec(Node root){
         if(root != null){
             printTreeRec(root.left);
-            System.out.println(root.data);
             printTreeRec(root.right);
+            System.out.println(root.data);
         }
     }
+
 
     public static void main(String[] args){
         MyBinaryTree tree = new MyBinaryTree();
@@ -65,7 +126,9 @@ class MyBinaryTree {
         tree.insert(27);
         tree.insert(11);
         tree.insert(73);
+        //tree.printTree();
 
+        tree.deleteData(50);
         tree.printTree();
     }
 
