f969dd4b4f2d760a6fe8ef5e94ab45da26a196ae
evan-mitchell93 <30278906+evan-mitchell93@users.noreply.github.com>
2021-04-30 14:55:42 -0400

    random circle and rectangle
    draw random color, size, position circles and rectangles when "c" and "r" are released on the keyboard

--- RandomShapes/MyFrame.java
+++ RandomShapes/MyFrame.java
@@ -1,9 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements KeyListener {
     
+    ArrayList<Rectangle> rects;
+    ArrayList<Circle> circles;
+
     public MyFrame(){
+        this.rects = new ArrayList<Rectangle>();
+        this.circles = new ArrayList<Circle>();
+        addKeyListener(this);
         setTitle("Shapes");
         setSize(400,400);
         setVisible(true);
@@ -12,15 +22,53 @@ public class MyFrame extends JFrame {
 
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        Circle c1 = new Circle(20);
-        c1.setPosition(100,100);
-        c1.setColor(Color.ORANGE);
-        c1.draw(g2d);
+        //draw all the rectangles
+        for(Rectangle r : rects){
+            r.draw(g2d);
+        }
+        //draw all the circles
+        for(Circle c : circles){
+            c.draw(g2d);
+        }
+    }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
 
-        Rectangle r1 = new Rectangle(10,50);
-        r1.setColor(Color.RED);
-        r1.setPosition(220, 75);
-        r1.draw(g2d);
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //if r is released, create a new rectangle with random color, size, position
+        // add it to the the list of rectangles and redraw
+        if(e.getKeyCode() == KeyEvent.VK_R){
+            Random rnd = new Random();
+            int w = rnd.nextInt((100- 10) + 10);
+            int h = rnd.nextInt((100 - 10) + 10);
+            Rectangle r = new Rectangle(w, h);
+            r.setColor(new Color(rnd.nextFloat(), rnd.nextFloat(), rnd.nextFloat()));
+            r.setPosition(rnd.nextInt((300-10) + 10), rnd.nextInt((300 - 10) + 10));
+            this.rects.add(r);
+            repaint();
+
+        }
+        //if c is released create a new circle with random color,radius, position
+        //add it to the list of circles and redraw
+        if(e.getKeyCode() == KeyEvent.VK_C){
+            Random rnd = new Random();
+            int rad = rnd.nextInt((100- 10) + 10);
+            Circle c = new Circle(rad);
+            c.setColor(new Color(rnd.nextFloat(), rnd.nextFloat(), rnd.nextFloat()));
+            c.setPosition(rnd.nextInt((300-10) + 10), rnd.nextInt((300 - 10) + 10));
+            this.circles.add(c);
+            repaint();
+
+        }
+        
     }
 }
